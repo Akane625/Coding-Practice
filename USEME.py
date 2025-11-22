ac43ae57eda67ea706b8d81edc7c42e8202a0fa5
Akane625 <yutucthomdaniel3@gmail.com>
2025-11-22 14:40:29 +0800

    Create USEME.py

---
+++ USEME.py
@@ -0,0 +1,14 @@
+# Use this code to choose what to do (this is better for less stress)
+import random
+
+languages = ["Python", "Kotlin", "SQL", "Pandas+NumPy+Matplotlib", "PHP+HTML"]
+makers = ["ChatGPT", "Gemini", "Perplexity", "DeepSeek", "Grok"]
+
+language = [random.choice(languages) for i in range(2)]
+maker = random.choice(makers)
+question = random.randint(1, 2)
+
+print(f"Code in: {" and ".join(language)}")
+print(f"Prompts from: {maker}")
+print(f"Generate: {question} prompts")
+print(f"Finish in a week (7 days)")
